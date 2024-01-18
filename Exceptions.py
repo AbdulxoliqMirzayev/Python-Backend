307730ac15bdae5c679d127f68b4da66354827bd
Abdulxoliq Mirzayev <abdulxoliqm@gmail.com>
2024-01-18 14:01:42 +0500

    Create Exceptions.py

---
+++ Exceptions.py
@@ -0,0 +1,70 @@
+# yosh = input("Yoshingizni kiriting - ")
+# try:
+#  yosh = int(yosh)
+#  print(f"Siz {2024 - yosh} yilda tugilgansiz.")
+# except:
+#     print("Butun son kiritmadingiz.")
+# print("Dastur tugadi .")
+
+
+
+# yosh = input("Yoshingizni kiriting - ")
+# try:
+#  yosh = int(yosh)
+# except ValueError:
+#     print("Butun son kiritmadingiz.")
+# else:
+#     print(f"Siz {2024 - yosh} yilda tugilgansiz.")
+# print("Dastur tugadi .")
+
+# while True:
+#     yosh = input("Yoshingizni kiriting. ")
+#     if yosh.isdigit():
+#         yosh = int(yosh)
+#         break
+# print(f"Siz {2024 - yosh} yilda tugilgansiz.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
